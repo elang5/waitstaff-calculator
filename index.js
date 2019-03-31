31abf9a9da323c31bd7a71bbf4ac5ab309389513
Elan Green <green.elan5@gmail.com>
2019-03-31 18:57:18 -0500

    features working

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const calculator = {
   earnings: [
     {tipTotal: 0, mealCount: 0, averageTip: 0}
   ],
-  meal: {mealPrice: 0, taxRate: 0, tipPercentage: 0}
+  meal: [
+    {tip: 0, subtotal: 0, total: 0}
+  ]
 };
 
 function renderCalculator() {
@@ -27,9 +29,9 @@ function renderCalculator() {
     <div class="container2">
     <div class="charges">
       <h2 class = "form-title">Customer Charges</h2>
-      <span class = "js-subtotal">Subtotal 0.00</span></br>
-      <span class="js-tip">Tip 0.00</span></br>
-      <span class="js-total">Total 0.00</span></br>
+      <span class = "js-subtotal">Subtotal ${calculator.meal[0].subtotal.toFixed(2)}</span></br>
+      <span class="js-tip">Tip ${calculator.meal[0].tip.toFixed(2)}</span></br>
+      <span class="js-total">Total ${calculator.meal[0].total.toFixed(2)}</span></br>
     </div>
     <div class="earnings">
         <h2 class = "form-title">My Earnings Info</h2>
@@ -43,6 +45,19 @@ function renderCalculator() {
   `);
 }
 
+
+function updateMyEarningsInfo(mealPrice, tipPercentage) {
+  // each time a tip is entered, the tip total should be updated
+  calculator.earnings[0].tipTotal += (mealPrice * tipPercentage);
+  // each time a meal is entered, mealCount++
+  calculator.earnings[0].mealCount++;
+  // average tip is tip total divided by meal count
+  const averageTip = calculator.earnings[0].tipTotal / calculator.earnings[0].mealCount;
+  $('.js-tip-total').html(`Tip Total: ${calculator.earnings[0].tipTotal.toFixed(2)}`);
+  $('.js-meal-count').html(`Meal Count: ${calculator.earnings[0].mealCount}`);
+  $('.js-average-tip').html(`Average Tip Per Meal: ${averageTip.toFixed(2)}`);
+}
+
 function handleMealSubmit() {
   // this should capture the submission of each meal detail
   $('.details').submit(function(event) {
@@ -50,53 +65,44 @@ function handleMealSubmit() {
     const mealPrice = parseFloat($('.js-meal-price').val());
     const taxRate = parseFloat($('.js-tax-rate').val());
     const tipPercentage = parseFloat($('.js-tip-percentage').val());
-    const tip = mealPrice * tipPercentage;
-    const tax = mealPrice * taxRate;
-    const subtotal = mealPrice + tax;
-    const total = tip + subtotal;
-    $('.js-subtotal').text(`Subtotal ${subtotal.toFixed(2)}`);
-    $('.js-tip').text(`Tip ${tip.toFixed(2)}`);
-    $('.js-total').text(`Total ${total.toFixed(2)}`);
-    calculator.earnings[0].tipTotal = tip;
-    calculator.earnings[0].mealCount++;
-    $('.js-tip-total').text(`Tip Total: ${tip.toFixed(2)}`);
-    $('.js-meal-count').text(`Meal Count: ${calculator.earnings[0].mealCount}`);
-    const averageTip = calculator.earnings[0].tipTotal / calculator.earnings[0].mealCount;
-    $('.js-average-tip').text(`Average Tip Per Meal: ${averageTip.toFixed(2)}`);
+    calculateTotal(mealPrice, tipPercentage, taxRate);
+    updateMyEarningsInfo(mealPrice, tipPercentage);
+    $('.js-meal-price').val('');
+    $('.js-tax-rate').val('');
+    $('.js-tip-percentage').val('');
   });
 }
 
 function handleCancel() {
+  // this should only reset the meal details form
   $('.js-cancel-button').on('click', function(event) {
     renderCalculator();
   });
-  // this should only reset the meal details form
 }
 
-function calculateTotal() {
-  // subtotal = tax rate + base meal price
-  // subtotal + tip
-  // return total
-  // update spans accordingly
+function calculateTotal(mealPrice, tipPercentage, taxRate) {
+  calculator.meal[0].tip = mealPrice * tipPercentage;
+  const tax = mealPrice * taxRate;
+  calculator.meal[0].subtotal = mealPrice + tax;
+  calculator.meal[0].total = calculator.meal[0].subtotal + calculator.meal[0].tip;
+  $('.js-subtotal').html(`Subtotal ${calculator.meal[0].subtotal.toFixed(2)}`);
+  $('.js-tip').html(`Tip ${calculator.meal[0].tip.toFixed(2)}`);
+  $('.js-total').html(`Total ${calculator.meal[0].total.toFixed(2)}`);
 }
 
-function updateCustomerCharges() {
-  // update customer charges based on what was entered in handleMealSubmit
-}
-
-function updateMyEarningsInfo() {
-  // each time a tip is entered, the tip total should be updated
-  // each time a meal is entered, mealCount++
-  // average tip is tip total divided by meal count
-}
 
 function handleResetCalc() {
   // reset the form completely
   $('.js-reset-button').on('click', function(event) {
-    console.log('hello');
-    calculator.earnings[0].tipTotal = 0;
-    calculator.earnings[0].mealCount = 0;
-    calculator.earnings[0].averageTip = 0;
+    const defaultCalculator = {
+      earnings: [
+        {tipTotal: 0, mealCount: 0, averageTip: 0}
+      ],
+      meal: [
+        {tip: 0, subtotal: 0, total: 0}
+      ]
+    };
+    Object.assign(calculator, defaultCalculator);
     renderCalculator();
   });
 }
